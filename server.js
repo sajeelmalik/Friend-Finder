2c527ee3c7ff10f0d33444fe15829ab383419b21
Sajeel Malik <sajeel23@berkeley.edu>
2018-09-10 17:01:23 -0700

    set up server.js and file structure

---
+++ server.js
@@ -0,0 +1,19 @@
+//Sajeel Malik
+//Friend Finder Full-Stack Application
+
+//09/10/18
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
